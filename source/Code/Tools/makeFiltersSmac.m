1af8ddd764c9295e6690289c905eaeb8ff627690
Richard Anthony Fernandes <55104357+rfernand387@users.noreply.github.com>
2023-07-16 11:32:37 -0400

    Add files via upload

---
+++ source/Code/Tools/makeFiltersSmac.m
@@ -0,0 +1,42 @@
+% this script automatically parses smac coefficients into SL2PD
+% Filters_Smac.mat structure
+% Landsat 5 
+Filtres_Smac.Landsat5.B1.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat5_b1_CONT.dat');
+Filtres_Smac.Landsat5.B2.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat5_b2_CONT.dat');
+Filtres_Smac.Landsat5.B3.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat5_b3_CONT.dat');
+Filtres_Smac.Landsat5.B4.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat5_b4_CONT.dat');
+Filtres_Smac.Landsat5.B5.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat5_b5_CONT.dat');
+Filtres_Smac.Landsat5.B7.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat5_b7_CONT.dat');
+
+% Landsat 7
+Filtres_Smac.Landsat7.B1.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat7_b1_CONT.dat');
+Filtres_Smac.Landsat7.B2.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat7_b2_CONT.dat');
+Filtres_Smac.Landsat7.B3.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat7_b3_CONT.dat');
+Filtres_Smac.Landsat7.B4.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat7_b4_CONT.dat');
+Filtres_Smac.Landsat7.B5.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat7_b5_CONT.dat');
+Filtres_Smac.Landsat7.B7.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat7_b7_CONT.dat');
+
+% Landsat 8
+Filtres_Smac.Landsat8.B1.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat8_440_1.dat');
+Filtres_Smac.Landsat8.B2.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat8_490_1.dat');
+Filtres_Smac.Landsat8.B3.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat8_560_1.dat');
+Filtres_Smac.Landsat8.B4.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat8_660_1.dat');
+Filtres_Smac.Landsat8.B5.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat8_860_1.dat');
+Filtres_Smac.Landsat8.B6.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat8_1630_1.dat');
+Filtres_Smac.Landsat8.B7.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat8_2250_1.dat');
+Filtres_Smac.Landsat8.B8.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_Landsat8_PAN_1.dat');
+
+% Sentinel 2a
+Filtres_Smac.SENTINEL2.Band1.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B1.dat');
+Filtres_Smac.SENTINEL2.Band2.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B2.dat');
+Filtres_Smac.SENTINEL2.Band3.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B3.dat');
+Filtres_Smac.SENTINEL2.Band4.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B4.dat');
+Filtres_Smac.SENTINEL2.Band5.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B5.dat');
+Filtres_Smac.SENTINEL2.Band6.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B6.dat');
+Filtres_Smac.SENTINEL2.Band7.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B7.dat');
+Filtres_Smac.SENTINEL2.Band8.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B8.dat');
+Filtres_Smac.SENTINEL2.Band8a.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B8a.dat');
+Filtres_Smac.SENTINEL2.Band9.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B9.dat');
+Filtres_Smac.SENTINEL2.Band10.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B10.dat');
+Filtres_Smac.SENTINEL2.Band11.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B11.dat');
+Filtres_Smac.SENTINEL2.Band12.smac.cont = coefSMAC('F:\camryn\SL2PD-master\DATA\smac\smac-python.git\COEFS\Coef_S2A_CONT_B12.dat');
